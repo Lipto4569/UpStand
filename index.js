4246bee2fe2cb396bc2a926149591b1251983b10
Lipto4569 <gdeepto@gmail.com>
2026-04-25 15:45:42 +0530

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.querySelectorAll("nav>div>ul").forEach((e)=>{
+    e.setAttribute("class","ullist")
+})
+document.querySelectorAll("nav>div>ul>li").forEach((e)=>{
+    e.setAttribute("class","list")
+})
+document.getElementById("logo").style.marginRight="35vw"
+document.getElementById("logo").style.paddingRight="29vw"
