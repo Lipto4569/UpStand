34ce1be5b022786176661d56c1e42b635ff0f07e
Lipto4569 <gdeepto@gmail.com>
2026-04-26 20:41:12 +0530

    commit-3

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ document.querySelectorAll("nav>div>ul>li").forEach((e)=>{
 })
 document.getElementById("logo").style.marginRight="35vw"
 document.getElementById("logo").style.paddingRight="29vw"
+
+
+
+let location1= document.querySelector("#card1");
+let location2= document.querySelector("#card2");
+let location3= document.querySelector("#card3");
+let location4= document.querySelector("#card4");
+
